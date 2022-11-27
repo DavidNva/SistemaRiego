a9c87014400adf10627a03c1b08257db73160f16
David Nava <david.nava.garcia4@gmail.com>
2022-11-26 23:03:56 -0600

    SistemaRiegoCompleto: LCD, leds, sensorSuelo, Bluetooth, Arduino y APP Inventor Version 1.0

--- riegoConHumedad.ino
+++ riegoConHumedad.ino
@@ -1,17 +1,29 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);//nuevo
+
 int bomba = 8;
 int  humedad;
 int ledRojo = 13;
 int ledAzul = 12;
-int ledAmarillo = 11;
+int ledVerde = 11;
 
 int estado = 0;
 void setup()
 {
+  lcd.begin(0x3F,16,2);//nuevo
   Serial.begin(9600);
   pinMode(bomba,OUTPUT);  
   pinMode(A0, INPUT);
   pinMode(ledRojo, OUTPUT);
   pinMode(ledAzul, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  lcd.setCursor(0,0);
+  lcd.print("Iniciando...");
+  //lcd.print(humedad);
+  //lcd.print(" %");
+  lcd.setCursor(0,1);
+  lcd.print("Bomba Encendida ");
+  digitalWrite(ledVerde,HIGH);//Enciende el led verde
 }
 
 void loop()
@@ -26,11 +38,25 @@ void loop()
       digitalWrite(bomba, HIGH);
       digitalWrite(ledRojo,LOW);//
       digitalWrite(ledAzul,LOW);//
+      digitalWrite(ledVerde,LOW);//Apaga el led verde
+        lcd.setCursor(0,0);
+        lcd.print(" Bomba Apagada  ");
+        //lcd.print(humedad);
+        //lcd.print(" %");
+        lcd.setCursor(0,1);
+        lcd.print("               ");
       break;
       case 'A':
       digitalWrite(bomba, LOW);
       digitalWrite(ledRojo,LOW);//
       digitalWrite(ledAzul,LOW);//
+      digitalWrite(ledVerde,HIGH);//Enciende el led verde
+        lcd.setCursor(0,0);
+        lcd.print(" Bomba Encendida ");
+        //lcd.print(humedad);
+        //lcd.print(" %");
+        lcd.setCursor(0,1);
+        lcd.print("               ");
       break;
       case 'C':
             Serial.print(humedad);Serial.println (" %");
@@ -39,6 +65,16 @@ void loop()
             digitalWrite(bomba, LOW);
             digitalWrite(ledRojo,HIGH);
             digitalWrite(ledAzul,LOW);
+            digitalWrite(ledVerde,HIGH);//Apaga el led verde
+            lcd.setCursor(0,0);
+            lcd.print("Tierra Seca: ");
+            lcd.print(humedad);
+            lcd.print(" %");
+              //delay(1000);
+            lcd.setCursor(0,1);
+            lcd.print("Regando...     ");
+              //delay(1);
+              //lcd.clear();
           }
         if(humedad > 20){
             
@@ -46,6 +82,16 @@ void loop()
              digitalWrite(bomba, HIGH);
              digitalWrite(ledRojo,LOW);
             digitalWrite(ledAzul,HIGH);
+            digitalWrite(ledVerde,LOW);//Apaga el led verde
+            lcd.setCursor(0,0);
+            lcd.print("  Tierra Humeda  ");
+              //lcd.print(humedad);
+              //lcd.print(" %");
+              //delay(1000);
+            lcd.setCursor(0,1);
+              //lcd.print(humedad);
+            lcd.print("  Bomba Apagada ");
+              //delay(1);
           }
         delay(1000);
       break;
@@ -54,6 +100,12 @@ void loop()
       //estado = Serial.read();
       digitalWrite(ledRojo,LOW);//
       digitalWrite(ledAzul,LOW);//
+      digitalWrite(ledVerde,LOW);//Apaga el led verde
+      lcd.setCursor(0,0);
+      lcd.print("  Bomba Apagada ");
+      lcd.setCursor(0,1);
+      //lcd.print(humedad);
+      lcd.print("                    ");
       break;
       }
 
